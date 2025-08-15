25a31acc96fd066088bd8df14544088b5a6bca9b
oyatullohm <marziyahoni@gmail.com>
2025-08-15 05:04:36 +0500

    f

--- admin/urls.py
+++ admin/urls.py
@@ -1,7 +1,12 @@
 
+from django.urls import path,include , re_path
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
